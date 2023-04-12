4aec1878bb4da07faab26ef3dea3bb4594a0ba88
rafael <k.rafaelalves@gmail.com>
2023-04-12 19:43:35 -0300

    testes main

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,8 +1,6 @@
 package org.example;
 
-import org.example.dominio.Conteudo;
-import org.example.dominio.Curso;
-import org.example.dominio.Mentoria;
+import org.example.dominio.*;
 
 import java.time.LocalDate;
 
@@ -22,9 +20,32 @@ public class Main {
         conteudo.setDescricao("Descrição conteúdo");
         conteudo.setTitulo("Título conteúdo");
 
-        System.out.println(conteudo);
+//        System.out.println(conteudo);
+//        System.out.println(curso);
+//        System.out.println(mentoria);
 
-        System.out.println(curso);
-        System.out.println(mentoria);
+        Bootcamp bootcamp = new Bootcamp();
+        bootcamp.setNome("Bootcamp Java Dev");
+        bootcamp.setDescricao("Descrição Bootcamp Java Dev");
+        bootcamp.getConteudos().add(curso);
+        bootcamp.getConteudos().add(mentoria);
+
+        Dev devRafael = new Dev();
+        devRafael.setNome("Rafael");
+        devRafael.inscreverBootcamp(bootcamp);
+        devRafael.progredir();
+        devRafael.progredir();
+        System.out.println(devRafael.getConteudoInscritos());
+        System.out.println(devRafael.calcularTotalXp());
+        System.out.println(devRafael.getConteudosConcluidos());
+
+
+        Dev devDanielle = new Dev();
+        devDanielle.setNome("Danielle");
+        devDanielle.inscreverBootcamp(bootcamp);
+        System.out.println(devDanielle.calcularTotalXp());
+        System.out.println(devDanielle.getConteudoInscritos());
+        System.out.println(devDanielle.getConteudosConcluidos());
+
     }
 }
